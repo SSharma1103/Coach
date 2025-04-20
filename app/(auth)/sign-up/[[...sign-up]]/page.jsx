9d5175e29225cf93878849be8d284507bbed2009
shivam sharma <shivamsharma11032009@gmail.com>
2025-04-20 09:39:06 +0530

    wow

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp fallbackRedirectUrl="/Onboarding" />;
+  return <SignUp fallbackRedirectUrl="/onboarding" />;
 }
